b863fe8716470bf8c2ba5cbcf06eb7998fc084cc
PedroHenrique31 <pedrohcaraujo1@hotmail.com>
2019-09-22 17:29:19 -0300

    Correção de bugs com relação ao calculo do mdc.

--- Calculadora_Raiz.py
+++ Calculadora_Raiz.py
@@ -25,18 +25,23 @@ numero = 3#só pra não perder
 ###############################processamento############################################################################
     #TODO: melhorar essa interface
     ##Resultado=Calculator.raiz_quadrada(numero)
-    if(opcao==1):
+if(opcao==1):
         numero=int(input("Qual o número  desejas saber a raiz?"))
         Resultado=Calculator.calcula_raiz_limite(numero)
-    elif(opcao==2):
+        mdc='Não calculado!'
+elif(opcao==2):
         numero=int(input("Digite o valor que quer calcular\n"))
         grau=int(input("Digite o grau da raiz que deseja calcular\n(Apenas valores inteiros são aceitos)\n"))
         Resultado=Calculator.calcula_raiz_generica(numero,grau)
-    elif(opcao==3):
+        mdc = 'Não calculado!'
+elif(opcao==3):
         numero=int(input("Digite os valores que quer calcular\n"))
         grau=int(input("Agora o segundo numero:"))
         mdc=Calculator.mdc(numero,grau)
         Resultado=mdc
+else:
+        mdc='NÃO SELECIONADO!'
+        Resultado='NÃO SELECIONADO!'
 
 ################################ Saída #################################################################################
 #print("A raiz é ",Resultado)
